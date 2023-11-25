9ac6dddeefc5f3d25eb41b4f218487bf1a9f5e2b
Radim <58228032+Radim296@users.noreply.github.com>
2023-11-25 12:55:52 +0500

    main.py fix

--- python_fluent_scanner/__main__.py
+++ python_fluent_scanner/__main__.py
@@ -1,4 +1,13 @@
-from python_fluent_scanner.main import main
+from python_fluent_scanner.fluent_scanner import FluentScanner
 
-if __name__ == '__main__':
+
+def main() -> None:
+    """
+    The main function creates an instance of the FluentScanner class and calls its check method.
+    """
+    scanner = FluentScanner()
+    scanner.check()
+
+
+if __name__ == "__main__":
     main()
