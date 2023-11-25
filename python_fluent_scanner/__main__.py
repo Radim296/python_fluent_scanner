cc76ae774fe3d287b5e9b0d3d852df0b31321695
Radim <58228032+Radim296@users.noreply.github.com>
2023-11-25 13:02:47 +0500

    Package execution now works properly

--- python_fluent_scanner/__main__.py
+++ python_fluent_scanner/__main__.py
@@ -1,13 +1,4 @@
-from python_fluent_scanner.fluent_scanner import FluentScanner
-
-
-def main() -> None:
-    """
-    The main function creates an instance of the FluentScanner class and calls its check method.
-    """
-    scanner = FluentScanner()
-    scanner.check()
-
+from python_fluent_scanner.main import main
 
 if __name__ == "__main__":
     main()
